3907a7ff5170b03a1fd77553dca8db58fa89c23a
Patrícia <patricia.duarte.dds@gmail.com>
2023-01-26 15:22:23 -0300

    Inserindo item de menu.

---
+++ AppGallery/AppGallery/XamarinForms/Leiautes/PilhaLeiautes/Pilha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AppGallery.XamarinForms.Leiautes.PilhaLeiautes
+{
+    public class Pilha : ContentPage
+    {
+        public Pilha()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
